0a27631c516175635df10644b2d5fadb17da3363
Laura Brown <laurabrown1113@gmail.com>
2013-05-11 19:40:09 -0700

    setup rails

---
+++ ultragif/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ultragif::Application.config.secret_token = '9db0c710452f26015a5ba238e656aa8111933124dda9aa81b504c6248279ea26b0b57a3e85d8efe7ff5272d443163cddd8555bb88f3d7513d53e5a9acc61f1c3'
