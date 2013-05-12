3bd1676ab3d1b2c6ed3631cc976e2964acec7c3d
Moon Mayor <moonmayor@Moons-MacBook-Pro.local>
2013-05-11 20:07:23 -0700

    removing cowboy

--- js/canvases.js
+++
@@ -1,8 +0,0 @@
-function init(layers) {
-  for (var i = 0; i < layers.length; i++) {
-    var layer = document.getElementById(layers[i]);
-    background.width = WIDTH;
-    background.height = HEIGHT;
-    var ctx = background.getContext("2d");
-  }
-}
--- js/config.js
+++
@@ -1,9 +0,0 @@
-var FRAMERATE = 1000 / 60;
-
-var WIDTH = 800;
-var HEIGHT = 600;
-
-var SCALE = 1;
-
-var SMOOTH_SHOOTING = false;
-
