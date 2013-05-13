2bf2a2154583badb4815107e01b156428a54cb6b
Laura Brown <laurabrown1113@gmail.com>
2013-05-12 17:09:06 -0700

    mostly set up

--- app/assets/javascripts/UltraGifCtrl.js
+++ app/assets/javascripts/UltraGifCtrl.js
@@ -4,8 +4,8 @@ var UltraGifCtrl = function($scope) {
   $scope.canvas = new Canvas("untitled");
   $scope.layers = $scope.canvas.layers;
 
-  var dog = $scope.canvas.addLayer("img/1368390992733-dumpfm-BUTTPOISON-IMG_5432.jpg");
-  var face = $scope.canvas.addLayer("img/1368161387675-dumpfm-frederick-FUCK.png");
+  var dog = $scope.canvas.addLayer("images/dog.jpg");
+  var face = $scope.canvas.addLayer("images/face.png");
 
   face.rotate(-20);
   face.translate(-40, 0);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,16 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require angular
 //= require jquery
 //= require jquery_ujs
+//= require LZWEncoder
+//= require NeuQuant
+//= require GIFEncoder
 //= require dropzone
+//= require config
+//= require layer
+//= require canvas
+//= require ultragif
+//= require UltraGifCtrl
 //= require_tree .
